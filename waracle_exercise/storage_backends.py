7afa921aaca5e53e75df54ae5cdb9c6499686f56
Dan Bridson <briddo@gmail.com>
2021-11-11 11:03:30 +0000

    More dev

--- waracle_exercise/storage_backends.py
+++ waracle_exercise/storage_backends.py
@@ -2,12 +2,17 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3Boto3Storage):
+class SecurityTokenWorkaroundS3Boto3Storage(S3Boto3Storage):
+    def _get_security_token(self):
+        return None
+
+
+class StaticStorage(SecurityTokenWorkaroundS3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
 
 
-class PublicMediaStorage(S3Boto3Storage):
+class PublicMediaStorage(SecurityTokenWorkaroundS3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
